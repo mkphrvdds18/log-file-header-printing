9deb852a79f2939e78b9c4918f6efe06bf0a508e
mkphrvdds18 <29808555+mkphrvdds18@users.noreply.github.com>
2018-07-18 20:31:29 -0400

    Update log_file_header.py

--- log_file_header.py
+++ log_file_header.py
@@ -1,27 +1,31 @@
 
-# -*- coding: utf-8 -*-
 """
-Created on Fri Jun  1 10:03:48 2018
+# -*- coding: utf-8 -*-
 
+# goto working dir os.chdir("dir path here")
+# os.chdir("D:\\any_dir\\folder_for_logfile")
 
-# goto working dir 
 # log_file_name.txt will be created if not pre-exist
-# if the file exits 'a' will append new information to it
+# if the file exits option 'a' will append new information to it
+# if you like to make filename based on time, please check another post here
 
+"""
 import time
 import os 
 
 # print current date and time to the top of log file
+# use file.write() to add more lines , information etc
+
 
 file = open('log_file_name.txt','a')
 for i in range(1):
     file.write('#'*80)
     file.write('\n')
     file.write('Today\'s date is: ')
-    #file.write(str(today))  #use casting as date object is not str
     file.write(time.ctime())
     file.write('\n')
     file.write('\n')
 
 file.close()
+
 
